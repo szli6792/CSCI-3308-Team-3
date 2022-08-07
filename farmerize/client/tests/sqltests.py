f48d262245846af869e143cab77bbe9f8a76efb9
sko-code-malone <mafo8956@colorado.edu>
2022-08-07 11:57:41 -0600

    tests update 3

---
+++ farmerize/client/tests/sqltests.py
@@ -0,0 +1,79 @@
+import unittest
+import pymssql
+
+class TextprocTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_products(self):
+        conn = pymssql.connect('ec2-54-183-139-130.us-west-1.compute.amazonaws.com', 'farmerize_sa', 'pass1234', 'master')
+        cursor = conn.cursor(as_dict=True)
+        cursor.execute('SELECT * FROM farmerize.dbo.products')
+        item = cursor.fetchall()
+        conn.close()
+        self.assertEqual(len(item), 15)
+        self.assertEqual(item[0], {'ProductID': 1, 'FarmID': 1, 'Category': 'Fruit', 'Name': 'Apple', 'Img': 'apple.png', 'Price': 3.42, 'Description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nibh justo, aliquam vel scelerisque sed, ullamcorper ut neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra dolor.', 'About': 'Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit', 'Special': 0})
+    
+    def test_farms(self):
+        conn = pymssql.connect('ec2-54-183-139-130.us-west-1.compute.amazonaws.com', 'farmerize_sa', 'pass1234', 'master')
+        cursor = conn.cursor(as_dict=True)
+        cursor.execute('SELECT * FROM farmerize.dbo.farms')
+        item = cursor.fetchall()
+        conn.close()
+        self.assertEqual(item[0]['Name'], 'Oak Farms')
+        self.assertEqual(item[1]['Name'], 'High Ridge')
+        self.assertEqual(item[2]['Name'], 'Low Lands')
+        self.assertEqual(item[3]['Name'], 'Green Field')
+        self.assertEqual(item[4]['Name'], 'County Fresh')
+
+    def test_subscribe(self):
+        conn = pymssql.connect('ec2-54-183-139-130.us-west-1.compute.amazonaws.com', 'farmerize_sa', 'pass1234', 'master')
+        cursor = conn.cursor(as_dict=True)
+        cursor.execute("INSERT INTO farmerize.dbo.subscriptions (email) VALUES ('test@gmail.com')")
+        conn.commit()
+        cursor.execute('SELECT * FROM farmerize.dbo.subscriptions WHERE email=%s', 'test@gmail.com')
+        item = cursor.fetchone()
+        cursor.execute('DELETE FROM farmerize.dbo.subscriptions WHERE email=%s', 'test@gmail.com')
+        conn.commit()
+        conn.close()
+        self.assertEqual(item['Email'], 'test@gmail.com')
+
+    def test_users(self):
+        conn = pymssql.connect('ec2-54-183-139-130.us-west-1.compute.amazonaws.com', 'farmerize_sa', 'pass1234', 'master')
+        cursor = conn.cursor(as_dict=True)
+        cursor.execute("INSERT INTO farmerize.dbo.users (First, Last, Phone, Email, Address, Age, Password) VALUES ('Test', 'Test', '1231231234', 'test@gmail.com', '123 test lane', '22', 'Password')")
+        conn.commit()
+        cursor.execute('SELECT First, Last, Phone, Email, Address, Age, Password FROM farmerize.dbo.users WHERE email=%s', 'test@gmail.com')
+        item = cursor.fetchone()
+        cursor.execute('DELETE FROM farmerize.dbo.users WHERE First=%s', 'Test')
+        conn.commit()
+        conn.close()
+        self.assertEqual(item, {'First': 'Test', 'Last': 'Test', 'Phone': '1231231234', 'Email': 'test@gmail.com', 'Address': '123 test lane', 'Age': 22, 'Password': 'Password'})
+
+    def test_reviews(self):
+        conn = pymssql.connect('ec2-54-183-139-130.us-west-1.compute.amazonaws.com', 'farmerize_sa', 'pass1234', 'master')
+        cursor = conn.cursor(as_dict=True)
+        cursor.execute("INSERT INTO farmerize.dbo.reviews (ProductID, Review, Rating) VALUES (1, 'Test', '4')")
+        conn.commit()
+        cursor.execute('SELECT ProductID, Review, Rating FROM farmerize.dbo.reviews WHERE Review=%s', 'Test')
+        item = cursor.fetchone()
+        cursor.execute('DELETE FROM farmerize.dbo.reviews WHERE Review=%s', 'Test')
+        conn.commit()
+        conn.close()
+        self.assertEqual(item, {'ProductID': 1, 'Review': 'Test', 'Rating': 4})
+
+# Main: Run Test Cases
+if __name__ == '__main__':
+    unittest.main()
--- farmerize/client/tests/tests.py
+++ farmerize/client/tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.alert import Alert
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
 import pymssql
@@ -101,6 +102,68 @@ class TextprocTestCase(unittest.TestCase):
     #     self.assertEqual(image1.get_attribute('style'), 'background-image: url("/static/media/top_img.276d0fec076b8b16c9d9.png");')
     #     image2 = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div[2]/div[2]')
     #     self.assertEqual(image2.get_attribute('style'), 'background-image: url("/static/media/mid_img.3d020d982e658a0bf2bd.png");')
+    
+    #does not work
+    # def test_results_links(self):
+    #     driver = webdriver.Chrome(ChromeDriverManager().install())
+    #     driver.get('http://ec2-54-183-139-130.us-west-1.compute.amazonaws.com/')
+    #     delay = 3
+    #     test = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'card-name')))
+    #     driver.find_element(By.ID, 'search').send_keys('meat')
+    #     driver.find_element(By.CLASS_NAME, 'search_button').click()
+    #     count = 0
+    #     test = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'data_img')))
+    #     aboutus = driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[2]/div[2]/a[1]')
+    #     self.assertEqual(aboutus.get_attribute('href'), 'http://ec2-54-183-139-130.us-west-1.compute.amazonaws.com/aboutus')
+    #     myaccount = driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[2]/div[2]/a[2]')
+    #     self.assertEqual(myaccount.get_attribute('href'), 'http://ec2-54-183-139-130.us-west-1.compute.amazonaws.com/myaccount')
+
+    #doesnt work
+    def test_product_links(self):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.get('http://ec2-54-183-139-130.us-west-1.compute.amazonaws.com/')
+        delay = 3
+        test = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[2]/div[2]/div/div/div[1]/div[1]/div/div[6]/div[2]/a/button')))
+        driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div[2]/div/div/div[1]/div[1]/div/div[6]/div[2]/a').click()
+        aboutus = driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[2]/div[2]/a[1]')
+        self.assertEqual(aboutus.get_attribute('href'), 'http://ec2-54-183-139-130.us-west-1.compute.amazonaws.com/aboutus')
+        myaccount = driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[2]/div[2]/a[2]')
+        self.assertEqual(myaccount.get_attribute('href'), 'http://ec2-54-183-139-130.us-west-1.compute.amazonaws.com/myaccount')
+
+    # def test_user_create(self):
+    #     driver = webdriver.Chrome(ChromeDriverManager().install())
+    #     driver.get('http://ec2-54-183-139-130.us-west-1.compute.amazonaws.com/myaccount')
+    #     delay = 3
+    #     test = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'accountbutton')))
+    #     driver.find_element(By.XPATH, '//*[@id="firstName"]').send_keys('Test')
+    #     driver.find_element(By.XPATH, '//*[@id="lastName"]').send_keys('Test')
+    #     driver.find_element(By.XPATH, '//*[@id="phone"]').send_keys('1231231234')
+    #     driver.find_element(By.XPATH, '//*[@id="email"]').send_keys('test@gmail.com')
+    #     driver.find_element(By.XPATH, '//*[@id="address"]').send_keys('123 test lane')
+    #     driver.find_element(By.XPATH, '//*[@id="age"]').send_keys('22')
+    #     driver.find_element(By.XPATH, '//*[@id="password"]').send_keys('test')
+    #     button = driver.find_element(By.CLASS_NAME, 'accountbutton')
+    #     button.submit()
+    #     Alert(driver).accept()
+    #     conn = pymssql.connect('ec2-54-183-139-130.us-west-1.compute.amazonaws.com', 'farmerize_sa', 'pass1234', 'master')
+    #     cursor = conn.cursor(as_dict=True)
+    #     cursor.execute('SELECT First, Last, Phone, Email, Address, Age, Password FROM farmerize.dbo.users WHERE email=%s', 'test@gmail.com')
+    #     item = cursor.fetchone()
+    #     cursor.execute('DELETE FROM farmerize.dbo.users WHERE email=%s', 'test@gmail.com')
+    #     conn.commit()
+    #     conn.close()
+    #     self.assertEqual(item, {'First': 'Test', 'Last': 'Test', 'Phone': '1231231234', 'Email': 'test@gmail.com', 'Address': '123 test lane', 'Age': 22, 'Password': 'test'})
+
+    # def test_myaccount_links(self):
+    #     driver = webdriver.Chrome(ChromeDriverManager().install())
+    #     driver.get('http://ec2-54-183-139-130.us-west-1.compute.amazonaws.com/myaccount')
+    #     delay = 3
+    #     test = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="firstName"]')))
+    #     aboutus = driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[2]/div[2]/a[1]')
+    #     self.assertEqual(aboutus.get_attribute('href'), 'http://ec2-54-183-139-130.us-west-1.compute.amazonaws.com/aboutus')
+    #     myaccount = driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[2]/div[2]/a[2]')
+    #     self.assertEqual(myaccount.get_attribute('href'), 'http://ec2-54-183-139-130.us-west-1.compute.amazonaws.com/myaccount')    
+
 
 # Main: Run Test Cases
 if __name__ == '__main__':
