c197f1bd50d69087aa0cb5ea151127d41cb2b3e1
etbr8506 <97766930+etbr8506@users.noreply.github.com>
2022-08-07 22:56:43 -0400

    Update App.js

--- farmerize/client/src/App.js
+++ farmerize/client/src/App.js
@@ -12,6 +12,41 @@ import Footer from './components/Footer'
 import MyAccount from "./pages/MyAccount";
 import ResultsPage from './pages/Results';
 
+/* 
+PRIMARY STRUCTURE OF CODE BASE
+
+APP (This page): CSCI-3308-Team-3/farmerize/client/src/App.js --> Directory of pages within the website.
+
+PAGES: CSCI-3308-Team-3/farmerize/client/src/pages/ --> Contains all the website pages broken down into javascript (page.jsx) and css (page.css) files.
+- Home page: CSCI-3308-Team-3/farmerize/client/src/pages/Home.jsx /
+- About us page: CSCI-3308-Team-3/farmerize/client/src/pages/AboutUsPage.jsx /
+- Results page: CSCI-3308-Team-3/farmerize/client/src/pages/Results.jsx /
+- Product page: CSCI-3308-Team-3/farmerize/client/src/pages/Product.jsx /
+- Sign up page: CSCI-3308-Team-3/farmerize/client/src/pages/MyAccount.jsx /
+
+COMPONENTS: CSCI-3308-Team-3/farmerize/client/src/components/ --> Contains sections of code that is likely to be reused in multiple places to save space
+and time, broken down into javascript (page.jsx) and css (page.css) files.
+- Header bar: CSCI-3308-Team-3/farmerize/client/src/components/NavBar.jsx /
+- Footer bar: CSCI-3308-Team-3/farmerize/client/src/components/Footer.jsx /
+- Sub bar: CSCI-3308-Team-3/farmerize/client/src/components/SubBar.jsx /
+- Search bar: CSCI-3308-Team-3/farmerize/client/src/components/SearchBar.jsx /
+- Results bar: CSCI-3308-Team-3/farmerize/client/src/components/ResBar.jsx /
+- Card: CSCI-3308-Team-3/farmerize/client/src/components/Card.jsx /
+- Card row: CSCI-3308-Team-3/farmerize/client/src/components/CardRow.jsx /
+
+IMAGES: CSCI-3308-Team-3/farmerize/client/public/images/ --> Contains image files used throughout website dynamically.
+
+ASSETS: CSCI-3308-Team-3/farmerize/client/src/assets/ --> Contains image files used throughout website staticly.
+
+TESTS: CSCI-3308-Team-3/farmerize/client/tests/ --> Contains selenium Python testing files.
+
+SERVER: CSCI-3308-Team-3/farmerize/server/index.js / --> Contains code interfacing front end with database / back end.
+
+UTILITIES: CSCI-3308-Team-3/farmerize/client/src/utils/ --> Contains code connecting website to database.
+
+PROXY: CSCI-3308-Team-3/farmerize/client/src/setupProxy.js / --> Contains code connecting website to proxy server.
+*/
+
 const App = () => {
   return (
     // <div className="App">
